b4baa4e09f54010dea9fb8ae65ea6fb5ce8430ca
Akshayachu0005 <37728191+Akshayachu0005@users.noreply.github.com>
2018-03-24 11:45:51 +0530

    Add files via upload

---
+++ KIT/app/src/main/java/com/example/nikhilg_nikii/kit/Principal_Message.java
@@ -0,0 +1,13 @@
+package com.example.nikhilg_nikii.kit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal_Message extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal__message);
+    }
+}
